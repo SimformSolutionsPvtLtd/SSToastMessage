10755e27e5e47f3a6149bb7fad71530c5d7c7107
Ankit Panchal <ankit.p@simformsolutions.com>
2020-09-09 17:37:41 +0530

    Update colors of message view in example Add launch screen Update the Screenshots image Update gif

--- SSToastMessage/Example/SSToastMessage/ContentView.swift
+++ SSToastMessage/Example/SSToastMessage/ContentView.swift
@@ -21,18 +21,20 @@ struct MyButtonView : View {
             self.showing.toggle()
         }) {
             Text(title)
+                .font(.system(size: 18))
+                .fontWeight(.medium)
                 .foregroundColor(.black)
         }
     }
 }
 
 struct ContentView: View {
-    let bgColor = Color(hex: "D8F2F0")
-    let popupColor = Color(hex: "3d5a80")
-    let topToastColor = Color(hex: "293241")
-    let bottomToastColor = Color(hex: "98c1d9")
-    let topFloatColor = Color(hex: "BF5841")
-    let bottomFloatColor = Color(hex: "ee6c4d")
+    let bgColor = Color(hex: "E8F2F0")
+    let alertColor = Color(hex: "ee6c4d")
+    let topToastColor = Color(hex: "4067CC")
+    let bottomToastColor = Color(hex: "bfdcae")
+    let topFloatColor = Color(hex: "61A117")
+    let bottomFloatColor = Color(hex: "3d5a80")
 
     @State var showAlert = false
     @State var showTopToast = false
@@ -49,34 +51,41 @@ struct ContentView: View {
     }
 
     var body: some View {
-                VStack {
-                    VStack(spacing: 30) {
-                        MyButtonView(showing: self.$showAlert, title: "Alert", hideAll: self.dismissAll)
-                        MyButtonView(showing: self.$showTopToast, title: "Top Toast", hideAll: self.dismissAll)
-                        MyButtonView(showing: self.$showBottomToast, title: "Bottom Toast", hideAll: self.dismissAll)
-                        MyButtonView(showing: self.$showTopFloater, title: "Top Floater", hideAll: self.dismissAll)
-                        MyButtonView(showing: self.$showBottomFloater, title: "Bottom Floater", hideAll: self.dismissAll)
-                    }
-                    .present(isPresented: self.$showAlert, type: .alert, autohideDuration: nil, closeOnTap: false) {
-                        self.createAlertView()
-                    }
+        GeometryReader { geometryProxy in
+            VStack {
+                VStack(spacing: 30) {
+                    MyButtonView(showing: self.$showAlert, title: "Alert", hideAll: self.dismissAll)
+                    MyButtonView(showing: self.$showTopToast, title: "Top Toast", hideAll: self.dismissAll)
+                    MyButtonView(showing: self.$showBottomToast, title: "Bottom Toast", hideAll: self.dismissAll)
+                    MyButtonView(showing: self.$showTopFloater, title: "Top Floater", hideAll: self.dismissAll)
+                    MyButtonView(showing: self.$showBottomFloater, title: "Bottom Floater", hideAll: self.dismissAll)
+                }
+                
 
-                    .present(isPresented: self.$showTopToast, type: .toast, position: .top, onTap: {print("on toast tap")}) {
-                        self.createTopToastView()
-                    }
-                    .present(isPresented: self.$showBottomToast, type: .toast, position: .bottom, onTap: {print("on toast tap")}) {
-                        self.createBottomToastView()
-                    }
-                    .present(isPresented: self.$showTopFloater, type: .floater(), position: .top, animation: Animation.spring(), onTap: {print("on toast tap")}) {
-                        self.createTopFloaterView()
-                    }
-                    .present(isPresented: self.$showBottomFloater, type: .floater(), position: .bottom,animation: Animation.spring(), autohideDuration: nil, onTap: {print("on toast tap")}) {
-                        self.createBottomFloaterView()
-                    }
-
-            }
-                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-                .background(bgColor)
+                .present(isPresented: self.$showAlert, type: .alert, animation: Animation.interactiveSpring(), autohideDuration: nil, closeOnTap: false) {
+                    self.createAlertView()
+                }
+                    
+                .present(isPresented: self.$showTopToast, type: .toast, position: .top, onTap: {print("on toast tap")}) {
+                    self.createTopToastView()
+                }
+                .present(isPresented: self.$showBottomToast, type: .toast, position: .bottom, onTap: {print("on toast tap")}) {
+                    self.createBottomToastView()
+                }
+                .present(isPresented: self.$showTopFloater, type: .floater(), position: .top, animation: Animation.spring(), onTap: {print("on toast tap")}) {
+                    self.createTopFloaterView()
+                }
+                .present(isPresented: self.$showBottomFloater, type: .floater(), position: .bottom,animation: Animation.spring(), autohideDuration: nil, onTap: {print("on toast tap")}) {
+                    self.createBottomFloaterView()
+                }
+                
+                }
+            .frame(width: geometryProxy.size.width, height: geometryProxy.size.height)
+            
+        }
+        .background(self.bgColor)
+        .edgesIgnoringSafeArea(.all)
+             
 
 
     }
@@ -90,11 +99,13 @@ struct ContentView: View {
 
             Text("Hey There!!!")
                 .foregroundColor(.white)
+                .font(.system(size: 20))
                 .fontWeight(.bold)
 
             Text("Please click on dismiss to hide.")
-                .font(.system(size: 12))
-                .foregroundColor(Color(red: 0.9, green: 0.9, blue: 0.9))
+                .font(.system(size: 16))
+                .fontWeight(.medium)
+                .foregroundColor(.white)
 
             Spacer()
 
@@ -102,7 +113,7 @@ struct ContentView: View {
                 self.showAlert = false
             }) {
                 Text("Dismiss")
-                    .font(.system(size: 14))
+                    .font(.system(size: 16))
                     .foregroundColor(.black)
                     .fontWeight(.bold)
             }
@@ -112,14 +123,14 @@ struct ContentView: View {
         }
         .padding(EdgeInsets(top: 20, leading: 20, bottom: 40, trailing: 20))
         .frame(width: 300, height: 300)
-        .background(self.popupColor)
+        .background(self.alertColor)
         .cornerRadius(10.0)
         .shadow(color: Color(.sRGBLinear, white: 0, opacity: 0.13), radius: 10.0)
     }
 
     func createTopToastView() -> some View {
         VStack {
-            Spacer(minLength: 20)
+            Spacer(minLength: 30)
             HStack() {
                 Image("mike")
                     .resizable()
@@ -130,22 +141,23 @@ struct ContentView: View {
                 VStack(alignment: .leading, spacing: 2) {
                     HStack {
                         Text("Mike")
+                            .font(.system(size: 18))
                             .foregroundColor(.white)
                             .fontWeight(.bold)
                         Spacer()
                         Text("10:10")
                             .font(.system(size: 12))
-                            .foregroundColor(Color(red: 0.9, green: 0.9, blue: 0.9))
+                            .foregroundColor(.white)
                     }
 
-                    Text("Hey, Don't miss the WWDC on tonight at 10 AM PST.")
+                    Text("Great things never come from comfort zones.")
                         .lineLimit(2)
-                        .font(.system(size: 14))
+                        .font(.system(size: 16))
                         .foregroundColor(.white)
                 }
             }.padding(15)
         }
-        .frame(width: UIScreen.main.bounds.width, height: 110)
+        .frame(width: UIScreen.main.bounds.width, height: 130)
         .background(self.topToastColor)
     }
 
@@ -159,13 +171,14 @@ struct ContentView: View {
 
                 VStack(alignment: .leading, spacing: 2) {
                     Text("Whooohoo!!")
-                        .foregroundColor(.black)
+                        .font(.system(size: 16))
+                        .foregroundColor(Color(hex: "206a5d"))
                         .fontWeight(.bold)
 
-                    Text("Your order has been placed succesfully! Sit sight we deliver the food in 20 minutes.")
+                    Text("Your order has been placed succesfully! Sit tight we deliver the food in 20 minutes.")
                         .lineLimit(2)
                         .font(.system(size: 14))
-                        .foregroundColor(.black)
+                        .foregroundColor(Color(hex: "206a5d"))
                 }
             }
             Spacer(minLength: 10)
@@ -187,18 +200,20 @@ struct ContentView: View {
                 VStack(alignment: .leading, spacing: 2) {
                     HStack {
                         Text("Mike")
+                            .font(.system(size: 16))
                             .foregroundColor(.white)
                             .fontWeight(.bold)
                         Spacer()
                         Text("10:10")
                             .font(.system(size: 12))
-                            .foregroundColor(Color(red: 0.9, green: 0.9, blue: 0.9))
+                            .foregroundColor(.white)
                     }
-
-                    Text("Hey, Don't miss the WWDC on tonight at 10 AM PST.")
+                    
+                    Text("Hey, Welcome to Simform Solutions")
                         .lineLimit(2)
                         .font(.system(size: 14))
                         .foregroundColor(.white)
+                        
                 }
             }.padding(15)
         }
@@ -217,13 +232,13 @@ struct ContentView: View {
 
             VStack(alignment: .leading, spacing: 2) {
                 Text("Ever thought of taking a break from work and travel?")
-                    .foregroundColor(.black)
+                    .foregroundColor(.white)
                     .fontWeight(.bold)
                     .lineLimit(3)
 
                 Text("If you have ever thought of something like a career break or sabbatical, you need to realize that there will never be a perfect time.")
                     .font(.system(size: 14))
-                    .foregroundColor(.black)
+                    .foregroundColor(.white)
             }
         }
         .padding(15)
