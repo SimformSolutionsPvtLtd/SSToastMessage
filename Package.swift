efb3d912236a173f0098ca5aead3960fe65e03b9
YagnikBavishi <yagnik.b@simformsolutions.com>
2024-05-29 19:04:41 +0530

    Updated iOS required version in package.swift file

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SSToastMessage",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v14),
         .macOS(.v10_15)
     ],
     products: [
